4927fa5b737971aa9f45ac19a7a11f2dc4e3cc29
Bob Rinehart <rinebob111185@gmail.com>
2021-07-05 10:23:19 -0700

    now removing TOH exp external material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { TopHeroesComponent } from './heroes/top-heroes/top-heroes.component';
-import { HeroDetailComponent } from './heroes/hero-detail/hero-detail.component';
-import { HeroesComponent } from './heroes/heroes.component';
-// import { DashboardComponent } from './heroes/heroes/heroes.component';
-import { MaterialComponent } from './material/material.component';
-import { WelcomeComponent } from './welcome/welcome.component';
-// import { MainComponent } from './main/main/main.component';
 import { DashboardComponent } from './main/dashboard/dashboard.component';
 import { PositionsComponent } from './main/positions/positions.component';
 import { OptionChainComponent } from './main/option-chain/option-chain.component';
 import { IvRankComponent } from './main/iv-rank/iv-rank.component';
-import { ChartsComponent } from './charts/charts/charts.component';
 import { StrategiesComponent } from './strategies/strategies/strategies.component';
 import { ToolsComponent } from './tools/tools/tools.component';
 import { AccountComponent } from './admin/account/account.component';
 import { HelpComponent } from './admin/help/help.component';
 import { CalendarComponent } from './main/calendar/calendar.component';
+import { ChartsMainComponent } from './charts/charts-main/charts-main.component';
 
 const routes: Routes = [
 	{ path: '', redirectTo: '/dashboard', pathMatch: 'full' },
@@ -28,29 +21,12 @@ const routes: Routes = [
 	{ path: 'iv-rank', component: IvRankComponent },
 	{ path: 'option-chain', component: OptionChainComponent },
 	{ path: 'calendar', component: CalendarComponent },
-	{ path: 'charts', component: ChartsComponent },
+	{ path: 'charts', component: ChartsMainComponent },
 	{ path: 'strategies', component: StrategiesComponent },
 	{ path: 'tools', component: ToolsComponent },
 	{ path: 'account', component: AccountComponent },
 	{ path: 'help', component: HelpComponent },
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	{ path: 'heroes', component: HeroesComponent, children: [
-		{ path: 'top-heroes', component: TopHeroesComponent },
-		{ path: 'detail/:id', component: HeroDetailComponent },
-	] },
-	{ path: 'material', component: MaterialComponent },
-	{ path: 'welcome', component: WelcomeComponent },
 
 ];
 
