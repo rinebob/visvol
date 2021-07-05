e79d2e67ea960f5fc0d1a597b8e6c04fc5ab9686
Bob Rinehart <rinebob111185@gmail.com>
2021-07-05 15:03:44 -0700

    Added remaining chart settings form controls and event emitter

--- src/app/charts/chart-settings/chart-settings.component.ts
+++ src/app/charts/chart-settings/chart-settings.component.ts
@@ -1,52 +1,20 @@
-import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Observable, Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { switchMap, takeUntil } from 'rxjs/operators';
 
+import { ChartSetting, ChartType, ScaleType, TimeFrame } from '../../common/interfaces';
 
-export interface ChartSetting {
-  ticker: string;
-  timeframe?: TimeFrame;
-  chartType?: ChartType;
-  scaleType?: ScaleType;
-  startDate?: Date;
-  endDate?: Date;
-
-}
-
-export enum TimeFrame {
-  ONE_MINUTE = '1m',
-  FIVE_MINUTE = '5m',
-  TEN_MINUTE = '10m',
-  SIXTY_MINUTE = '60m',
-  ONE_DAY = '1d',
-  ONE_WEEK = '1w',
-  ONE_MONTH = '1m',
-}
-
-export enum ChartType {
-  LINE = 'line',
-  BAR = 'bar',
-  CANDLESTICK = 'candlestick',
-}
-
-export enum ScaleType {
-  LINEAR = 'linear',
-  LOG = 'log',
-}
 
 const defaultChartSetting = {
   ticker: 'TSLA',
   timeFrame: TimeFrame.ONE_DAY,
   chartType: ChartType.LINE,
   scaleType: ScaleType.LINEAR,
-  // startDate: new Date(),
-  // endDate: new Date(),
+  startDate: new Date(),
+  endDate: new Date(),
 
 };
-
-
-
 
 @Component({
   selector: 'vz-chart-settings',
@@ -55,37 +23,48 @@ const defaultChartSetting = {
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ChartSettingsComponent implements OnInit, OnDestroy {
+  @Output() chartSettings = new EventEmitter<ChartSetting>();
   readonly destroy = new Subject<void>();
   readonly timeframes = Object.values(TimeFrame);
+  readonly chartTypes = Object.values(ChartType);
+  readonly scaleTypes = Object.values(ScaleType);
   readonly settingsForm: FormGroup;
+  settingsFormValues: ChartSetting = defaultChartSetting;
 
-  chartSettings$: Observable<ChartSetting>;
+  // chartSettings$: Observable<ChartSetting>;
 
   constructor() { 
     this.settingsForm = new FormGroup({
       tickerControl: new FormControl('TSLA'),
-      // timeframeControl: new FormControl(TimeFrame.ONE_DAY),
-      // chartTypeControl: new FormControl(ChartType.LINE),
-      timeframeControl: new FormControl('1D'),
-      chartTypeControl: new FormControl('line'),
+      timeframeControl: new FormControl(TimeFrame.ONE_DAY),
+      chartTypeControl: new FormControl(ChartType.LINE),
+      scaleTypeControl: new FormControl(ScaleType.LINEAR),
+      // tickerControl: new FormControl(''),
+      // timeframeControl: new FormControl(''),
+      // chartTypeControl: new FormControl(''),
+      // scaleTypeControl: new FormControl(''),
       // startDateControl: new FormControl(''),
       // endDateControl: new FormControl(''),
   
     });
     
-    this.chartSettings$ = this.settingsForm.valueChanges.pipe(
+    this.settingsForm.valueChanges.pipe(
       takeUntil(this.destroy)
-    )
+    ).subscribe(values => {
+      this.settingsFormValues = values;
+      console.log('cS ctor t.sFV values: ', values);
+      this.chartSettings.emit(values);
+    });
   }
 
   ngOnInit() {
-    this.settingsForm.patchValue({
-      'tickerControl': defaultChartSetting.ticker,
-      'timeframeControl': defaultChartSetting.timeFrame,
-      'chartTypeControl': defaultChartSetting.chartType,
-      'scaleTypeControl': defaultChartSetting.scaleType,
+    // this.settingsForm.patchValue({
+    //   'tickerControl': defaultChartSetting.ticker,
+    //   'timeframeControl': defaultChartSetting.timeFrame,
+    //   'chartTypeControl': defaultChartSetting.chartType,
+    //   'scaleTypeControl': defaultChartSetting.scaleType,
 
-    });
+    // });
   }
 
   ngOnDestroy () {
